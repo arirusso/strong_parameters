ce8d960938686595d38411239c1209d1acb0615d
Ari Russo <ari.russo@gmail.com>
2014-10-03 14:24:45 -0400

    comments, formatting

--- lib/action_controller/parameters.rb
+++ lib/action_controller/parameters.rb
@@ -11,6 +11,11 @@ module ActionController
       @permitted = false
     end
 
+    # If the requested method exists in the strong parameters instance module,
+    # include the module
+    # @param [String, Symbol] method_name
+    # @param [Boolean] include_private
+    # @return [Boolean]
     def respond_to_missing?(method_name, include_private = false)
       if StrongParameters.method_defined?(:method)
         self.class.send(:include, StrongParameters)
@@ -18,12 +23,19 @@ module ActionController
       super
     end
 
+    # If the require method is called, include the strong parameters module.
+    # This is necessary because the name require conflicts with Object(Kernel)#require
     def require(*args)
       self.class.send(:include, StrongParameters)
       super
     end
 
-    # Lazily add strong params
+    # If the requested method exists in the strong parameters instance module,
+    # include the module
+    # @param [String, Symbol] method_name
+    # @param [*Object] args
+    # @param [&Proc] block
+    # @return [Object]
     def method_missing(method, *args, &block)
       if StrongParameters.method_defined?(:method)
         self.class.send(:include, StrongParameters)
@@ -51,139 +63,141 @@ module ActionController
     end
   end
 
+  # Strong parameter instance methods
   module StrongParameters
 
-      def self.included(base)
-        base.send(:attr_accessor, :permitted)
-        base.send(:alias_method, :permitted?, :permitted)
-        base.send(:alias_method, :required, :require)
-        base.send(:cattr_accessor, :action_on_unpermitted_parameters, :instance_accessor => false)
+    # Add the stuff normally done via class methods 
+    def self.included(base)
+      base.send(:attr_accessor, :permitted)
+      base.send(:alias_method, :permitted?, :permitted)
+      base.send(:alias_method, :required, :require)
+      base.send(:cattr_accessor, :action_on_unpermitted_parameters, :instance_accessor => false)
+    end
+
+    # Never raise an UnpermittedParameters exception because of these params
+    # are present. They are added by Rails and it's of no concern.
+    NEVER_UNPERMITTED_PARAMS = %w( controller action )
+
+    def permit!
+      each_pair do |key, value|
+        convert_hashes_to_parameters(key, value)
+        self[key].permit! if self[key].respond_to? :permit!
       end
 
-      # Never raise an UnpermittedParameters exception because of these params
-      # are present. They are added by Rails and it's of no concern.
-      NEVER_UNPERMITTED_PARAMS = %w( controller action )
+      @permitted = true
+      self
+    end
 
-      def permit!
-        each_pair do |key, value|
-          convert_hashes_to_parameters(key, value)
-          self[key].permit! if self[key].respond_to? :permit!
-        end
+    def require(key)
+      self[key].presence || raise(ActionController::ParameterMissing.new(key))
+    end
 
-        @permitted = true
-        self
-      end
+    def permit(*filters)
+      params = self.class.new
 
-      def require(key)
-        self[key].presence || raise(ActionController::ParameterMissing.new(key))
-      end
+      filters.each do |filter|
+        case filter
+        when Symbol, String then
+          params[filter] = self[filter] if has_key?(filter)
+          keys.grep(/\A#{Regexp.escape(filter.to_s)}\(\d+[if]?\)\z/).each { |key| params[key] = self[key] }
+        when Hash then
+          self.slice(*filter.keys).each do |key, value|
+            return unless value
 
-      def permit(*filters)
-        params = self.class.new
+            key = key.to_sym
 
-        filters.each do |filter|
-          case filter
-          when Symbol, String then
-            params[filter] = self[filter] if has_key?(filter)
-            keys.grep(/\A#{Regexp.escape(filter.to_s)}\(\d+[if]?\)\z/).each { |key| params[key] = self[key] }
-          when Hash then
-            self.slice(*filter.keys).each do |key, value|
-              return unless value
+            params[key] = each_element(value) do |value|
+              # filters are a Hash, so we expect value to be a Hash too
+              next if filter.is_a?(Hash) && !value.is_a?(Hash)
 
-              key = key.to_sym
+              value = self.class.new(value) if !value.respond_to?(:permit)
 
-              params[key] = each_element(value) do |value|
-                # filters are a Hash, so we expect value to be a Hash too
-                next if filter.is_a?(Hash) && !value.is_a?(Hash)
-
-                value = self.class.new(value) if !value.respond_to?(:permit)
-
-                value.permit(*Array.wrap(filter[key]))
-              end
+              value.permit(*Array.wrap(filter[key]))
             end
           end
         end
-
-        unpermitted_parameters!(params) if self.class.action_on_unpermitted_parameters
-
-        params.permit!
       end
 
-      def [](key)
-        convert_hashes_to_parameters(key, super)
-      end
+      unpermitted_parameters!(params) if self.class.action_on_unpermitted_parameters
 
-      def fetch(key, *args)
-        convert_hashes_to_parameters(key, super)
-      rescue KeyError, IndexError
-        raise ActionController::ParameterMissing.new(key)
-      end
+      params.permit!
+    end
 
-      def slice(*keys)
-        self.class.new(super).tap do |new_instance|
-          new_instance.instance_variable_set :@permitted, @permitted
-        end
-      end
+    def [](key)
+      convert_hashes_to_parameters(key, super)
+    end
 
-      def dup
-        duplicate = Parameters.new(self)
-        duplicate.instance_variable_set :@permitted, @permitted
-        duplicate
-      end
+    def fetch(key, *args)
+      convert_hashes_to_parameters(key, super)
+    rescue KeyError, IndexError
+      raise ActionController::ParameterMissing.new(key)
+    end
 
-      protected
-      def convert_value(value)
-        if value.class == Hash
-          self.class.new(value)
-        elsif value.is_a?(Array)
-          value.dup.replace(value.map { |e| convert_value(e) })
-        else
-          value
-        end
-      end
-
-      private
-      def convert_hashes_to_parameters(key, value)
-        if value.is_a?(Parameters) || !value.is_a?(Hash)
-          value
-        else
-          # Convert to Parameters on first access
-          self[key] = self.class.new(value)
-        end
-      end
-
-      def each_element(object)
-        if object.is_a?(Array)
-          object.map { |el| yield el }.compact
-          # fields_for on an array of records uses numeric hash keys
-        elsif object.is_a?(Hash) && object.keys.all? { |k| k =~ /\A-?\d+\z/ }
-          hash = object.class.new
-          object.each { |k,v| hash[k] = yield v }
-          hash
-        else
-          yield object
-        end
-      end
-
-      def unpermitted_parameters!(params)
-        return unless self.class.action_on_unpermitted_parameters
-
-        unpermitted_keys = unpermitted_keys(params)
-
-        if unpermitted_keys.any?
-          case self.class.action_on_unpermitted_parameters
-          when :log
-            ActionController::Base.logger.debug "Unpermitted parameters: #{unpermitted_keys.join(", ")}"
-          when :raise
-            raise ActionController::UnpermittedParameters.new(unpermitted_keys)
-          end
-        end
-      end
-
-      def unpermitted_keys(params)
-        self.keys - params.keys - NEVER_UNPERMITTED_PARAMS
+    def slice(*keys)
+      self.class.new(super).tap do |new_instance|
+        new_instance.instance_variable_set :@permitted, @permitted
       end
     end
+
+    def dup
+      duplicate = Parameters.new(self)
+      duplicate.instance_variable_set :@permitted, @permitted
+      duplicate
+    end
+
+    protected
+    def convert_value(value)
+      if value.class == Hash
+        self.class.new(value)
+      elsif value.is_a?(Array)
+        value.dup.replace(value.map { |e| convert_value(e) })
+      else
+        value
+      end
+    end
+
+    private
+    def convert_hashes_to_parameters(key, value)
+      if value.is_a?(Parameters) || !value.is_a?(Hash)
+        value
+      else
+        # Convert to Parameters on first access
+        self[key] = self.class.new(value)
+      end
+    end
+
+    def each_element(object)
+      if object.is_a?(Array)
+        object.map { |el| yield el }.compact
+        # fields_for on an array of records uses numeric hash keys
+      elsif object.is_a?(Hash) && object.keys.all? { |k| k =~ /\A-?\d+\z/ }
+        hash = object.class.new
+        object.each { |k,v| hash[k] = yield v }
+        hash
+      else
+        yield object
+      end
+    end
+
+    def unpermitted_parameters!(params)
+      return unless self.class.action_on_unpermitted_parameters
+
+      unpermitted_keys = unpermitted_keys(params)
+
+      if unpermitted_keys.any?
+        case self.class.action_on_unpermitted_parameters
+        when :log
+          ActionController::Base.logger.debug "Unpermitted parameters: #{unpermitted_keys.join(", ")}"
+        when :raise
+          raise ActionController::UnpermittedParameters.new(unpermitted_keys)
+        end
+      end
+    end
+
+    def unpermitted_keys(params)
+      self.keys - params.keys - NEVER_UNPERMITTED_PARAMS
+    end
+  end
 end
 
 ActionController::Base
